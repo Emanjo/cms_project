cd341c0fc04c059b9456461ddecc77e3c224435b
Emil Johannessen <emil.jona@gmai.com>
2018-12-03 23:28:12 +0100

    Small adjustments

--- cms1/myPortofolio/single.php
+++ cms1/myPortofolio/single.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 <main>
-      <div class="container">
-        <div class="single-post">
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php if ( has_post_thumbnail() ) : ?>
-              <img src="<?php the_post_thumbnail_url(); ?>" title="<?php the_title_attribute(); ?>">
-            <?php endif; ?>
-          <h1><?php the_title(); ?></h1>
-          <small><?php the_time('F j, Y'); ?> at <?php the_time('G:i'); ?></small>
-            <?php the_content(); ?>
-        <?php endwhile;  endif; ?>
-        </div>
+    <div class="container">
+      <div class="single-post">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <?php if ( has_post_thumbnail() ) : ?>
+            <img src="<?php the_post_thumbnail_url(); ?>" title="<?php the_title_attribute(); ?>">
+          <?php endif; ?>
+        <h1><?php the_title(); ?></h1>
+        <small><?php the_time('F j, Y'); ?> at <?php the_time('G:i'); ?></small>
+          <?php the_content(); ?>
+      <?php endwhile;  endif; ?>
       </div>
-    </main>
-    <?php get_footer(); ?>
+    </div>
+  </main>
+<?php get_footer(); ?>
